46963aa53091a31913a2581f7398b9c595b24d45
Linder <linder.lopezr@gmail.com>
2026-07-08 21:35:27 -0500

    Avance: Completados 10 ejercicios

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,9 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+fn call_me() {
+    // Puedes dejar el cuerpo vacío o agregar algo
+}
+
 fn main() {
     call_me(); // Don't change this line
 }
